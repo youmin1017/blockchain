a6871855f3533356b92e80954fb76ec00723c0fa
youmin1017 <xx4016481@gmail.com>
2024-04-12 20:02:54 +0800

    support dotenv to initial account

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.SEED_PHRASE,
+      },
+      chainId: 1337,
+    },
+  },
 };
 
 export default config;
