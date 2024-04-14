e9a8c8e5cea309d596c7028aaeab031f8c0ca406
youmin1017 <xx4016481@gmail.com>
2024-04-15 02:39:23 +0800

    test: faucet

---
+++ test/faucet.ts
@@ -0,0 +1,33 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Faucet", function () {
+  async function deployFaucetFixture() {
+    const lockedAmount = ethers.parseEther("1000");
+
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Faucet = await ethers.getContractFactory("Faucet");
+    const faucet = await Faucet.deploy({ value: lockedAmount });
+
+    return { faucet, lockedAmount, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      const { faucet, owner, otherAccount, lockedAmount } = await loadFixture(deployFaucetFixture);
+
+      expect(await faucet.owner()).to.equal(owner.address);
+    });
+
+    it("Should able to withdraw the funds", async function () {
+      const { faucet, owner, otherAccount, lockedAmount } = await loadFixture(deployFaucetFixture);
+
+      const res = await faucet.connect(otherAccount).withdraw(ethers.parseEther("1"));
+
+      expect(await ethers.provider.getBalance(otherAccount)).to.greaterThan(ethers.parseEther("10000"));
+    });
+  });
+});
